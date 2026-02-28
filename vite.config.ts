6cfbafe935c18b1a3defad95f28e5a2066f9efba
Krister Myrman <krippakrull@gmail.com>
2026-02-28 21:13:45 +0100

    chore: scaffold SvelteKit project with dependencies

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		}),
+		tailwindcss(),
+		sveltekit()
+	]
+});
